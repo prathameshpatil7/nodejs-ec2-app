e1e7c276bb695ec5dd350af3b839d9654a88681b
rwireprathamesh570 <prathamesh.patil@researchwire.in>
2024-11-18 22:38:30 +0530

    update

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
 import express from "express";
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 
-// import db from "./mongoC.js";
+import db from "./mongoC.js";
 
-// const port = 4000;
-// const app = express();
-// //api
-
-// app.use((_req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-
-//   next();
-// });
-
-// // Parses the text as url encoded data
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Parses the text as json
-// app.use(bodyParser.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World, from express");
-// });
-
-// app.post("/addUser", async (req, res) => {
-//   let collection = await db.collection("users");
-//   let newDocument = req.body;
-//   newDocument.date = new Date();
-//   let result = await collection.insertOne(newDocument);
-//   console.log("rreq" + req.body);
-//   res.send(result).status(204);
-// });
-
-// app.get("/getUsers", async (req, res) => {
-//   let collection = await db.collection("users");
-//   let results = await collection
-//     .find({})
-
-//     .toArray();
-//   res.send(results).status(200);
-// });
-
-// app.listen(port, function () {
-//   console.log("Server is listening at port:" + port);
-// });
-
-// const express = require("express");
+const port = 9000;
 const app = express();
-const port = 9000; // Use port 80 for HTTP traffic
+//api
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Hello from EC2 API!" });
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`API server is running on http://localhost:${port}`);
+// Parses the text as url encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parses the text as json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World, from express");
+});
+
+app.post("/addUser", async (req, res) => {
+  let collection = await db.collection("users");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  console.log("rreq" + req.body);
+  res.send(result).status(204);
+});
+
+app.get("/getUsers", async (req, res) => {
+  let collection = await db.collection("users");
+  let results = await collection
+    .find({})
+
+    .toArray();
+  res.send(results).status(200);
+});
+
+app.listen(port, function () {
+  console.log("Server is listening at port:" + port);
 });
