6589e282d661b20cea09efc5cf780a8d020a2e3d
rwireprathamesh570 <prathamesh.patil@researchwire.in>
2024-11-19 11:11:06 +0530

    update

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
-const URI = process.env.MONGO_URI || "";
+// import dotenv from "dotenv";
+// dotenv.config();
+const URI = `mongodb+srv://patilprathamesh18et1003:${process.env.MONGO_PASSWORD}@dev-cluster.aykft.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`;
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
